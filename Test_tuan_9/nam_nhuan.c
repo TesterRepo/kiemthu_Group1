c1065cbca5ecc8b02fbd572d41069a915d7eae7f
chinhnm57 <chinh211194@gmail.com>
2015-11-09 23:20:55 +0700

    test tuan 9
    test tuan 9

---
+++ Test_tuan_9/nam_nhuan.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+#include "math.h"
+#include "conio.h"
+int main()
+{
+    int n;
+    printf("Nhap vao nam bat ky ");
+    scanf("%d",&n);
+    if(((n%4==0)&&(n%100!=0))||(n%400==0))
+        printf(" Nam da nhap la nam nhuan ");
+    else
+        printf(" Khong phai la nam nhuan ");
+    getch();
+    return 0;
+}
